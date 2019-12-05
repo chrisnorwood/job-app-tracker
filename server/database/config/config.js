820bf25bf5a624252cf0ee2f43c42c5c0ae0092d
Chris Norwood <cnorwood92@gmail.com>
2019-12-04 21:22:21 -0800

    Update sequelize config, make note of DB setup

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,17 +1,19 @@
-{
-  "development": {
-    "url": process.env.DEV_DATABASE_URL,
-    "dialect": "postgres",
-    "operatorsAliases": false
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: "postgres",
+    operatorsAliases: false
   },
-  "test": {
-    "url": process.env.TEST_DATABASE_URL,
-    "dialect": "postgres",
-    "operatorsAliases": false
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: "postgres",
+    operatorsAliases: false
   },
-  "production": {
-    "url": process.env.DATABASE_URL,
-    "dialect": "postgres",
-    "operatorsAliases": false
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    operatorsAliases: false
   }
 }
