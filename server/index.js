a9f976987cd210615dcd3c0f1f687ca7bc06a391
Chris Norwood <cnorwood92@gmail.com>
2019-12-05 08:21:26 -0800

    Add helmet

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const express = require('express');
+const helmet = require('helmet');
 const morgan  = require('morgan');
 
 const app = express();
 
-app.use(morgan('tiny'));
+// Middleware
+app.use(helmet());
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get('/health', (req, res, next) => {
   res.send('OK!');
