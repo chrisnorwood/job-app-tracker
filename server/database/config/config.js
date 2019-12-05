9ed0eda27ee4e13142d043bfa380392fbd688d4a
Chris Norwood <cnorwood92@gmail.com>
2019-12-04 20:56:59 -0800

    Setup basic sequelize configuration and structure

---
+++ server/database/config/config.js
@@ -0,0 +1,17 @@
+{
+  "development": {
+    "url": process.env.DEV_DATABASE_URL,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "url": process.env.TEST_DATABASE_URL,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "url": process.env.DATABASE_URL,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express from "express";
-import morgan from "morgan";
+const express = require('express');
+const morgan  = require('morgan');
 
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.json());
 
-app.get("/health", (req, res, next) => {
-  res.send("OK!");
+app.get('/health', (req, res, next) => {
+  res.send('OK!');
 });
 
 const port = process.env.PORT || 3000;
